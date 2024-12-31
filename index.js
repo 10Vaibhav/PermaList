c1591fb22fbfda089b5503755b432c2a2542ed47
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-12-31 20:05:42 +0530

    Complete Permalist

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const db = new pg.Client({
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port
+})
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -12,23 +27,61 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+async function getData(){
+  const result = await db.query("SELECT * FROM items;");
+  return result.rows
+}
+
+
+app.get("/", async(req, res) => {
+
+  const items = await  getData()
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async(req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  // items.push({ title: item });
+
+  try{
+    await db.query("INSERT INTO items (title) VALUES ($1)",[item]);
+  }catch (error){
+    console.log(error);
+  }
+
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async(req, res) => {
 
-app.post("/delete", (req, res) => {});
+  const id = req.body.updatedItemId;
+  const title = req.body.updatedItemTitle;
+
+  try{
+    await db.query("UPDATE items SET title = $1 WHERE id = $2;", [title,id]);
+    res.redirect("/");
+  }catch (error){
+    console.log(error);
+  }
+});
+
+app.post("/delete", async(req, res) => {
+
+  const deleteItemId = req.body.deleteItemId;
+
+  try{
+    await db.query("DELETE FROM items where id = $1",[deleteItemId]);
+    res.redirect("/");
+  }catch (error){
+    console.log(error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
